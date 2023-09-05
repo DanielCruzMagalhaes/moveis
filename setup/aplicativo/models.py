10b12c6415e2b493812e857b0135ef1f64e7ca7b
DanielCruzMagalhaes <danielcruzmagalhaes@gmail.com>
2023-09-05 20:33:36 -0300

    feitas as migrations

--- setup/aplicativo/models.py
+++ setup/aplicativo/models.py
@@ -11,22 +11,22 @@ class Banner(models.Model):
     
 
 class Main (models.Model):
-    titulo2 = models.CharField(max_length=200)
-    description2 =models.CharField(max_length=200)
-    imagem2 = models.ImageField(upload_to="image")
+    titulo = models.CharField(max_length=200)
+    description =models.CharField(max_length=200)
+    imagem = models.ImageField(upload_to="image")
 
 class Main2 (models.Model):
-    titulo3 = models.CharField(max_length=200)
-    description3 =models.CharField(max_length=200)
-    imagem3 = models.ImageField(upload_to="image")
+    titulo = models.CharField(max_length=200)
+    description =models.CharField(max_length=200)
+    imagem = models.ImageField(upload_to="image")
     icones =models.ImageField(upload_to="image")
 
 
 class footer (models.Model):
-    titulo4 = models.CharField(max_length=200)
-    description4 =models.CharField(max_length=200)
-    imagem4 = models.ImageField(upload_to="image")
-    icones2 =models.ImageField(upload_to="image")
+    titulo = models.CharField(max_length=200)
+    description =models.CharField(max_length=200)
+    imagem = models.ImageField(upload_to="image")
+    icones =models.ImageField(upload_to="image")
 
 
 

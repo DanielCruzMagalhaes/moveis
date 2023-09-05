e5b7319244a9f938e86b604fd13b17180728132e
DanielCruzMagalhaes <danielcruzmagalhaes@gmail.com>
2023-09-05 19:37:24 -0300

    fazendo as migrations

--- setup/aplicativo/models.py
+++ setup/aplicativo/models.py
@@ -4,3 +4,29 @@ from django.db import models
 
 
 # Create your models here.
+class Banner(models.Model):
+    titulo = models.CharField(max_length=200)
+    description =  models.CharField(max_length=200)
+    imagem = models.ImageField(upload_to="Imagem")
+    
+
+class Main (models.Model):
+    titulo2 = models.CharField(max_length=200)
+    description2 =models.CharField(max_length=200)
+    imagem2 = models.ImageField(upload_to="image")
+
+class Main2 (models.Model):
+    titulo3 = models.CharField(max_length=200)
+    description3 =models.CharField(max_length=200)
+    imagem3 = models.ImageField(upload_to="image")
+    icones =models.ImageField(upload_to="image")
+
+
+class footer (models.Model):
+    titulo4 = models.CharField(max_length=200)
+    description4 =models.CharField(max_length=200)
+    imagem4 = models.ImageField(upload_to="image")
+    icones2 =models.ImageField(upload_to="image")
+
+
+

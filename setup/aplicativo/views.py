459cb39d63eda54539709dabcb58844f2bbf98d7
DanielCruzMagalhaes <danielcruzmagalhaes@gmail.com>
2023-09-05 21:11:16 -0300

    Finalizado

--- setup/aplicativo/views.py
+++ setup/aplicativo/views.py
@@ -7,4 +7,5 @@ from .models import *
 def index(request):
     banner = Banner.objects.get(id=1)
     main = Main.objects.all()
-    return render(request,'index.html',{"banner":banner,"main":main})
+    main2 = Main2.objects.all()
+    return render(request,'index.html',{"banner":banner,"main":main,"main2":main2,})
